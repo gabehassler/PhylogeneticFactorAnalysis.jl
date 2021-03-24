09bc94f2bc5b1439ddec8b6bf2981d974c38e2a1
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-03-24 16:28:58 -0700

    updated test script

--- test/test.jl
+++ test/test.jl
@@ -17,8 +17,9 @@ end
 
 data_path = joinpath(@__DIR__, "test_data.csv")
 newick_path = joinpath(@__DIR__, "test_newick.txt")
+labels_path = joinpath(@__DIR__, "test_labels.csv")
 
-dummy_files = [data_path, newick_path]
+dummy_files = [data_path, newick_path, labels_path]
 
 # throw_error_if_exists.(dummy_files)
 
@@ -38,6 +39,21 @@ end
 
 CSV.write(data_path, data)
 
+@assert iseven(P) # needed for pretty label test
+ordered_labels = Vector{String}(undef, P)
+erng = (div(P, 2) + 1):P
+orng = 1:div(P, 2)
+ordered_labels[orng] .= ["trait$i" for i = 1:2:P]
+ordered_labels[erng] .= ["trait$i" for i = P:-2:2]
+cats = fill("", P)
+cats[erng] .= "even"
+cats[orng] .= "odd"
+labels_df = DataFrame(trait = ordered_labels,
+                    pretty = [x * " pretty" for x in ordered_labels],
+                    cat = cats)
+CSV.write(labels_path, labels_df)
+
+
 write(newick_path, writeTopology(rtree(taxa, ultrametric=true)))
 
 iid_msp = PhylogeneticFactorAnalysis.ModelSelectionProvider([1, 2, 3], Float64[], 2)
@@ -55,18 +71,20 @@ final_mcmc = MCMCOptions()
 iid_input = PhylogeneticFactorAnalysis.PipelineInput(
             "iid",
             data_path, newick_path, iid_msp, iid_prior,
-            selection_mcmc = selection_mcmc, overwrite = true)
+            selection_mcmc = selection_mcmc, overwrite = true,
+            labels_path = labels_path)
 
 shrink_input = PhylogeneticFactorAnalysis.PipelineInput(
             "shrink",
             data_path, newick_path, shrinkage_msp, shrinkage_prior,
-            selection_mcmc = selection_mcmc)
+            selection_mcmc = selection_mcmc, overwrite=true)
 
 try
-    PhylogeneticFactorAnalysis.run_pipeline(iid_input)
-    PhylogeneticFactorAnalysis.run_pipeline(shrink_input)
-
+    # PhylogeneticFactorAnalysis.run_pipeline(iid_input)
+    # PhylogeneticFactorAnalysis.run_pipeline(shrink_input)
+    iid_input.tasks.make_selection_xml = false
     iid_input.tasks.run_selection_xml = false
+    iid_input.tasks.run_final_xml = false
     PhylogeneticFactorAnalysis.run_pipeline(iid_input)
 catch e
     @error "Something went wrong" exception=(e, catch_backtrace())
