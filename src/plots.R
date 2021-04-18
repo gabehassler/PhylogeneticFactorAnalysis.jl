795f80acaeab679ff0a0bea3de7630885f9ac8ab
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-04-18 11:44:58 -0700

    better plotting for large trees

--- src/plots.R
+++ src/plots.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(wesanderson)
 library(colorspace)
+library(tidyr)
 
 f1 <- function(x) {
   return(x^2.5)
@@ -188,7 +189,7 @@ plot_tree <- function(tree, colors,
     if (!is.na(new_labels)) {
       p <- p %<+% new_labels + geom_tiplab(aes(label=genus), offset=0.02)
     } else {
-      p <- p + geom_tiplab(aligh=TRUE)
+      p <- p + geom_tiplab(align=TRUE)
     }
   } else {
     # p <- p + geom_tiplab(aes(label=character(1)), align=TRUE)
@@ -202,9 +203,9 @@ plot_tree <- function(tree, colors,
 }
 
 plot_factor_tree <- function(name, tree_path, factors_path, factors = NA,
-                             classes = NA,
-                             height=20,
-                             width=20,
+                             class_path = NA,
+                             height=NA,
+                             width=NA,
                              border=FALSE,
                              line_width=1.0,
                              tip_labels=TRUE,
@@ -228,15 +229,38 @@ plot_factor_tree <- function(name, tree_path, factors_path, factors = NA,
   n <- length(taxa)
   x <- scale(x)
   
+
+  
   combined_layout = layout
+  if (is.na(height)) {
+    height <- 20
+    if (layout == "rectangular") {
+      height <- 0.1 * n
+    }
+  }
+  
+  if (is.na(width)) {
+    width <- 20
+  }
   if (layout=="circular") {
     combined_layout = "fan"
   }
   
-  include_class = !all(is.na(classes))
-  class_colors = discrete_scale("fill", "manual", colorRampPalette(brewer.pal(min(nrow(unique(classes)), 12), "Set3")), name=colnames(classes)[1]) #scale_fill_brewer(palette="Set2")
+  include_class = !is.na(class_path)
+  if (include_class) {
+    
+    # stop()
+    class_df = read.csv(class_path)
+    class_df[,2] <- as.factor(class_df[,2])
+    classes <- data.frame(x = class_df[,2])
+    colnames(classes) <- c(colnames(class_df)[2])
+    row.names(classes) <- class_df$taxon
+  }
+  class_fills = discrete_scale("fill", "manual", colorRampPalette(brewer.pal(min(nrow(unique(classes)), 12), "Set3")), name=colnames(classes)[1]) #scale_fill_brewer(palette="Set2")
+  class_colors = discrete_scale("color", "manual", colorRampPalette(brewer.pal(min(nrow(unique(classes)), 12), "Set3")), name=colnames(classes)[1])
   if (!all(is.na(class_palette))) {
-    class_colors = scale_fill_manual(values=class_palette, name=colnames(classes)[1])
+    class_fills = scale_fill_manual(values=class_palette, name=colnames(classes)[1])
+    class_colors = scale_color_manual(values=class_palette, name=colnames(classes)[1])
   }
   
   n_factors = length(factors)
@@ -257,12 +281,12 @@ plot_factor_tree <- function(name, tree_path, factors_path, factors = NA,
     pname <- paste(name, "_factors.svg", sep="")
     p2 <- p1
     if (include_class) {
-      p2 <- gheatmap(p1, classes, offset=0, width=heat_width, colnames_angle=90, colnames=TRUE, legend_title=colnames(classes)[1]) + class_colors
+      p2 <- my_gheatmap(p1, classes, offset=0, width=heat_width, colnames_angle=90, colnames=TRUE, legend_title=colnames(classes)[1]) + class_fills + class_colors
     }
     
-    p3 <- p2 + new_scale_fill()
-    p4 <- gheatmap(p3, x, offset=heat_width * 2, width=heat_width * n_factors, colnames_angle=90, colnames_offset_y = 0, legend_title="factor value") +
-      scale_fill_gradient2(midpoint = 0.0, low="blue", mid='grey', high="red") + labs(fill="factor value")
+    p3 <- p2 + new_scale_fill() + new_scale_color()
+    p4 <- my_gheatmap(p3, x, offset=heat_width * 2, width=heat_width * n_factors, colnames_angle=90, colnames_offset_y = 0, legend_title="factor value") +
+      scale_fill_gradient2(midpoint = 0.0, low="blue", mid='grey', high="red") + scale_color_gradient2(midpoint = 0.0, low="blue", mid='grey', high="red") + labs(fill="factor value")
     svg(pname, height=height, width=width)
     print(p4)
     dev.off()
@@ -280,7 +304,7 @@ plot_factor_tree <- function(name, tree_path, factors_path, factors = NA,
       pname <- paste(name, k, ".svg", sep="")
       
       if (include_class) {
-        p <- gheatmap(p, classes, offset=0, width=heat_width, colnames=FALSE, legend_title=colnames(classes)[1]) + class_colors
+        p <- my_gheatmap(p, classes, offset=0, width=heat_width, colnames=FALSE, legend_title=colnames(classes)[1]) + class_fills + class_colors
         # scale_fill_manual(values=class_palette, name=colnames(classes)[1])
         # scale_x_ggtree() +
         # scale_y_continuous(expand=c(0, 0.3))
@@ -299,4 +323,127 @@ plot_factor_tree <- function(name, tree_path, factors_path, factors = NA,
 }
 
 
+my_gheatmap <- function(p, data, offset=0, width=1, low="green", high="red", color="white",
+                     colnames=TRUE, colnames_position="bottom", colnames_angle=0, colnames_level=NULL,
+                     colnames_offset_x = 0, colnames_offset_y = 0, font.size=4, family="", hjust=0.5, legend_title = "value") {
+  
+  colnames_position %<>% match.arg(c("bottom", "top"))
+  variable <- value <- lab <- y <- NULL
+  
+  ## if (is.null(width)) {
+  ##     width <- (p$data$x %>% range %>% diff)/30
+  ## }
+  
+  ## convert width to width of each cell
+  width <- width * (p$data$x %>% range(na.rm=TRUE) %>% diff) / ncol(data)
+  
+  isTip <- x <- y <- variable <- value <- from <- to <- NULL
+  
+  ## handle the display of heatmap on collapsed nodes
+  ## https://github.com/GuangchuangYu/ggtree/issues/242
+  ## extract data on leaves (& on collapsed internal nodes) 
+  ## (the latter is extracted only when the input data has data on collapsed
+  ## internal nodes)
+  df <- p$data
+  nodeCo <- intersect(df %>% filter(is.na(x)) %>% 
+                        select(.data$parent, .data$node) %>% unlist(), 
+                      df %>% filter(!is.na(x)) %>% 
+                        select(.data$parent, .data$node) %>% unlist())
+  labCo <- df %>% filter(.data$node %in% nodeCo) %>% 
+    select(.data$label) %>% unlist()
+  selCo <- intersect(labCo, rownames(data))
+  isSel <- df$label %in% selCo
+  
+  df <- df[df$isTip | isSel, ]
+  start <- max(df$x, na.rm=TRUE) + offset
+  
+  dd <- as.data.frame(data)
+  ## dd$lab <- rownames(dd)
+  i <- order(df$y)
+  
+  ## handle collapsed tree
+  ## https://github.com/GuangchuangYu/ggtree/issues/137
+  i <- i[!is.na(df$y[i])]
+  
+  lab <- df$label[i]
+  ## dd <- dd[lab, , drop=FALSE]
+  ## https://github.com/GuangchuangYu/ggtree/issues/182
+  dd <- dd[match(lab, rownames(dd)), , drop = FALSE]
+  
+  
+  dd$y <- sort(df$y)
+  dd$lab <- lab
+  ## dd <- melt(dd, id=c("lab", "y"))
+  dd <- gather(dd, variable, value, -c(lab, y))
+  
+  i <- which(dd$value == "")
+  if (length(i) > 0) {
+    dd$value[i] <- NA
+  }
+  if (is.null(colnames_level)) {
+    dd$variable <- factor(dd$variable, levels=colnames(data))
+  } else {
+    dd$variable <- factor(dd$variable, levels=colnames_level)
+  }
+  V2 <- start + as.numeric(dd$variable) * width
+  mapping <- data.frame(from=dd$variable, to=V2)
+  mapping <- unique(mapping)
+  
+  dd$x <- V2
+  dd$width <- width
+  dd[[".panel"]] <- factor("Tree")
+  height <- 1.0
+  size <- 0.5
+  if (is.null(color)) {
+    p2 <- p + geom_tile(data=dd, aes(x, y, fill=value, color=value), width=width, height=height, size=size, inherit.aes=FALSE)
+  } else {
+    p2 <- p + geom_tile(data=dd, aes(x, y, fill=value, color=value), width=width, height=height, size=size, inherit.aes=FALSE)
+  }
+  if (is(dd$value,"numeric")) {
+    p2 <- p2 + scale_fill_gradient(low=low, high=high, na.value=NA, name = legend_title) # "white")
+    p2 <- p2 + scale_color_gradient(low=low, high=high, na.value=NA, name = legend_title) # "white")
+  } else {
+    p2 <- p2 + scale_fill_discrete(na.value=NA, name = legend_title) #"white")
+    p2 <- p2 + scale_color_discrete(na.value=NA, name = legend_title) #"white")
+  }
+  
+  if (colnames) {
+    if (colnames_position == "bottom") {
+      y <- 0
+    } else {
+      y <- max(p$data$y) + 1
+    }
+    mapping$y <- y
+    mapping[[".panel"]] <- factor("Tree")
+    p2 <- p2 + geom_text(data=mapping, aes(x=to, y = y, label=from), size=font.size, family=family, inherit.aes = FALSE,
+                         angle=colnames_angle, nudge_x=colnames_offset_x, nudge_y = colnames_offset_y, hjust=hjust)
+  }
+  
+  p2 <- p2 + theme(legend.position="right")
+  ## p2 <- p2 + guides(fill = guide_legend(override.aes = list(colour = NULL)))
+  
+  if (!colnames) {
+    ## https://github.com/GuangchuangYu/ggtree/issues/204
+    p2 <- p2 + scale_y_continuous(expand = c(0,0))
+  }
+  
+  attr(p2, "mapping") <- mapping
+  return(p2)
+}
 
+# aqui_dir = "C:\\Users\\gabeh\\OneDrive\\SD_storage\\many_traits\\public\\integrated_factors\\data\\mammalian_life_history"
+# newick = file.path(aqui_dir, "mammals_newick_processed.txt")
+# facs = file.path(aqui_dir, "mammals", "mammals_factorMeans.csv")
+# class_path <- file.path(aqui_dir, "mammals_classification.csv")
+# # aqui_dir = "C:\\Users\\gabeh\\OneDrive\\SD_storage\\many_traits\\public\\integrated_factors\\data\\aquilegia"
+# # newick = file.path(aqui_dir, "aquilegia_newick.txt")
+# # facs = file.path(aqui_dir, "aquilegiaBinary", "aquilegiaBinary_factorMeans.csv")
+# # class_path = file.path(aqui_dir, "aquilegia_pollinators - Copy.csv")
+# 
+# plot_factor_tree("test", newick, facs, tip_labels = FALSE, layout="circular", height=40, width=40, class_path = class_path)
+# 
+# class_df = read.csv(class_path)
+# class_df[,2] <- as.factor(class_df[,2])
+# classes <- data.frame(x = class_df[,2])
+# colnames(classes) <- c(colnames(class_df)[2])
+# # row.names(classes) <- class_df$taxon
