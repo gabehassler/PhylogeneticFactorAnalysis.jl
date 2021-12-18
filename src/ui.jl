4912d0d2b5028b7b71d7199e87a2cbe5d998acf1
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-12-18 15:49:45 -0800

    fixing ui issue

--- src/ui.jl
+++ src/ui.jl
@@ -28,15 +28,15 @@ end
 function pfa(;name::String,
             data_path::String,
             newick_path::String,
-            partition_seed::Int = -1,
-            mcmc_seed::Int = -1,
-            overwrite::Bool = true,
+            partition_seed::Int = Int(rand(UInt32)),
+            mcmc_seed::Int = Int(rand(UInt32)),
+            overwrite::Bool = false,
             standardize_traits::Bool = false,
             loadings_prior::String = "iid",
             loadings_constraint::String = "orthogonal",
             factors::Vector{Int} = [2],
             discrete_indices = Int[])
-    data = TraitsAndTree(data_path, newick_path, discrete_inds = discrete_indices)
+    data = TraitsAndTree(data_path, abspath(newick_path), discrete_inds = discrete_indices)
     model_options = ModelOptions(standardize_data = standardize_traits)
     if loadings_prior == "iid"
         prior = IIDPrior(loadings_constraint)
@@ -45,7 +45,7 @@ function pfa(;name::String,
     end
 
 
-    model_selection = ModelSelectionProvider(factors, nothing, 1)
+    model_selection = ModelSelectionProvider(factors, Float64[], 1)
 
     input = PipelineInput(name, data, model_selection, prior,
                          model_options = model_options,
@@ -60,6 +60,7 @@ function pfa(;name::String,
     catch e
         cd(old_wd)
         @error "Something went wrong" exception=(e, catch_backtrace())
+        error("An error occured, terminating. See stacktrace above for error details.")
     end
 end
 
