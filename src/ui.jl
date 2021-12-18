805fdf0ec3bfe87d7efb62ef3224c63f2227a1ff
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-12-17 16:31:40 -0800

    circumventing xml for programmatically running pfa

--- src/ui.jl
+++ src/ui.jl
@@ -17,3 +17,42 @@ function pfa(input_file::String)
         @error "Something went wrong" exception=(e, catch_backtrace())
     end
 end
+
+function pfa(name::String;
+                   data_path::String,
+                   newick_path::String,
+                   partition_seed::Int = -1,
+                   mcmc_seed::Int = -1,
+                   overwrite::Bool = true,
+                   standardize_traits::Bool = false,
+                   loadings_prior::String = "iid",
+                   loadings_constraint::String = "orthogonal",
+                   factors::Vector{Int} = [2],
+                   discrete_indices = Int[])
+    data = TraitsAndTree(data_path, newick_path, discrete_inds = discrete_indices)
+    model_options = ModelOptions(standardize_data = standardize_traits)
+    if loadings_prior == "iid"
+        prior = IIDPrior(loadings_constraint)
+    else
+        error("Unknown or unimplemented prior type")
+    end
+
+
+    model_selection = ModelSelectionProvider(factors, nothing, 1)
+
+    input = PipelineInput(name, data, model_selection, prior,
+                         model_options = model_options,
+                         julia_seed = partition_seed,
+                         beast_seed = mcmc_seed,
+                         overwrite = overwrite
+                         )
+
+    old_wd = pwd()
+    try
+        run_pipeline(input)
+    catch e
+        cd(old_wd)
+        @error "Something went wrong" exception=(e, catch_backtrace())
+    end
+end
+
